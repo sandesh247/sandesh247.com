45707484d546a2a0fce0e1cc0d88e78fd2f1027d
sandesh247 <sandesh247@gmail.com>
2009-05-10 17:00:51 +0530

    added google analytics tracker code.

--- index.php
+++ index.php
@@ -196,5 +196,14 @@
     <div id="ft" role="contentinfo" class="center"></div>
   </div>
 </body>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8543747-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
 </html>
 
